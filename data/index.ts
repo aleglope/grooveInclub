8cb6b658a217834fe9e7865dc151a7343ad9f622
AGonzL <agonzlopez.11@gmail.com>
2024-07-03 14:33:16 +0200

    Base de la aplicación

---
+++ data/index.ts
@@ -0,0 +1,194 @@
+export const navItems = [
+  { name: "Sobre Nosotros", link: "#about" },
+  { name: "Eventos", link: "#eventos" },
+  { name: "Nuestra Familia", link: "#testimonials" },
+  { name: "Contacto", link: "#contact" },
+];
+
+export const gridItems = [
+
+  {
+    id: 2,
+    title: " Siente la conexión global en nuestras fiestas.",
+    description: "La música une al mundo y Groove lo hace posible.",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-2 lg:min-h-[60vh]",
+    imgClassName: "",
+    titleClassName: "justify-top",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "y mas....",
+    description: "Techno,Trap,House,Electro",
+    className: "flex lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-end",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 5,
+    title: "Brenlla",
+    description: "DJ Residente",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/djbrenlla.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "¿Te gustaría un lugar reservado?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "DJ Brenlla",
+    des: "calle juan 123",
+    img: "/sanxenxo.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://ticketrona.com/",
+  },
+  {
+    id: 2,
+    title: "DJ Pepo",
+    des: "calle juan 123",
+    img: "/sanxenxo1.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Fatima Hajji",
+    des: "calle juan 123",
+    img: "/sanxenxo2.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/gymare.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "¡Las fiestas Groove son increíbles! La energía, la música y la gente crean una experiencia única. Es como estar en una celebración mundial, donde todos se conectan a través del ritmo. ¡Definitivamente una noche para recordar!",
+    name: "María G.",
+    title: "Rave Girl",
+  },
+  {
+    quote:
+      "¡Las fiestas Groove son increíbles! La energía, la música y la gente crean una experiencia única. Es como estar en una celebración mundial, donde todos se conectan a través del ritmo. ¡Definitivamente una noche para recordar!",
+    name: "María G.",
+    title: "Rave Girl",
+  },
+  {
+    quote:
+      "¡Las fiestas Groove son increíbles! La energía, la música y la gente crean una experiencia única. Es como estar en una celebración mundial, donde todos se conectan a través del ritmo. ¡Definitivamente una noche para recordar!",
+    name: "María G.",
+    title: "Rave Girl",
+  },
+  {
+    quote:
+      "¡Las fiestas Groove son increíbles! La energía, la música y la gente crean una experiencia única. Es como estar en una celebración mundial, donde todos se conectan a través del ritmo. ¡Definitivamente una noche para recordar!",
+    name: "María G.",
+    title: "Rave Girl",
+  },
+  {
+    quote:
+      "¡Las fiestas Groove son increíbles! La energía, la música y la gente crean una experiencia única. Es como estar en una celebración mundial, donde todos se conectan a través del ritmo. ¡Definitivamente una noche para recordar!",
+    name: "María G.",
+    title: "Rave Girl",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Mas contenido",
+    desc: "Mas contenido",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "mas contenido",
+    desc: "mas contenido",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "mas contenido",
+    desc: "mas contenido",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "mas contenido",
+    desc: "mas contenido",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
